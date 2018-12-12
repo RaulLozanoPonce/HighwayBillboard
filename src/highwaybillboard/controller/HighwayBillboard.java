028ef454f4075b557749db27918cb5f9bb2d9b32
raull <raull@DESKTOP-VFAHBAN>
2018-12-12 16:58:43 +0000

    Highway Billboard problem with commentaries. Algorithm: Tabulation/Memoization Version: 1.1

--- src/highwaybillboard/controller/HighwayBillboard.java
+++ src/highwaybillboard/controller/HighwayBillboard.java
@@ -13,33 +13,44 @@ public class HighwayBillboard {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         
         if(args.length==1){
+            //Distancia minima entre carteles
             int t = 10;
             double TInicio, TFin;
             switch (args[0]) {
                 case "-t":
                     System.out.println("Tabulation ---------------------------------------------------");
+                    //Cargamos el fichero con los datos
                     ExampleReaderTabulation et = new ExampleReaderTabulation();
                     et.read("ejemplo.txt");
                     int xt[] = et.getX();
                     int revenuet[] = et.getRevenue();
+                    //Calculamos el beneficio máximo
                     CalculateRevenueTabulation ct = new CalculateRevenueTabulation();
                     TInicio = System.currentTimeMillis();
+                    //Lo mostramos
                     System.out.println("El beneficio máximo es: " + ct.maxRevenue(xt, revenuet, t));
                     TFin = System.currentTimeMillis();
+                    //Mostramos el tiempo de ejecución
                     System.out.println("Tiempo de ejecución: " + (TFin - TInicio)/1000 + " microsegundos");
                     break;
                 case "-m":
                     System.out.println("Memoization --------------------------------------------------");
+                    //Cargamos el fichero con los datos
                     ExampleReaderMemoization em = new ExampleReaderMemoization();
                     em.read("ejemplo.txt");
                     ArrayList<Integer> xm = em.getX();
                     ArrayList<Integer> revenuem = em.getRevenue();
+                    //Calculamos el beneficio máximo
                     CalculateRevenueMemoization cm = new CalculateRevenueMemoization();  
+                    //Cargamos un posible mapa existente
                     cm.setMap(new File("map.map"));
                     TInicio = System.currentTimeMillis();
+                    //Mostramos el beneficio máximo
                     System.out.println("El beneficio máximo es: " + cm.maxRevenue(xm, revenuem, t));
                     TFin = System.currentTimeMillis();
+                    //Mostramos el tiempo de ejecución
                     System.out.println("Tiempo de ejecución: " + (TFin - TInicio)/1000 + " microsegundos");
+                    //Guardamos el estado del mapa
                     cm.saveMap(new File("map.map"));
                     break;
                 default:
--- src/highwaybillboard/model/CalculateRevenueMemoization.java
+++ src/highwaybillboard/model/CalculateRevenueMemoization.java
@@ -24,49 +24,49 @@ public class CalculateRevenueMemoization {
         if(map.containsKey(current_pos_key)){
             return map.get(current_pos_key);
         }else{
-            // Si tenemos un solo elemento, éste será el máximo revenue
+            //Si tenemos un solo elemento, éste será el máximo revenue
             if(x.size() == 1){
                 map.put(current_pos_key,revenue.get(0));
                 return map.get(current_pos_key);
             }
             
-            // Recorremos de arriba a abajo, tomamos última posición
+            //Recorremos de arriba a abajo, tomamos última posición
             int current_pos = x.get(x.size() - 1);
             
-            /* Obtenemos el max revenue hasta ahora, (del elemento actual para
-            abajo sin contar con el actual) */
+            /*Obtenemos el max revenue hasta ahora, (del elemento actual para
+            abajo sin contar con el actual)*/
             int current_max_rev = maxRevenue(cut(x,0,x.size()-1), cut(revenue,0,revenue.size()-1), min_distance);
             
-            // En las posiciones comprendidas entre 0 y la distancia mínima solo puede haber 1 billboard
+            //En las posiciones comprendidas entre 0 y la distancia mínima solo puede haber 1 billboard
             if(current_pos < min_distance){
                 map.put(current_pos_key, Math.max(revenue.get(revenue.size() - 1), current_max_rev));
                 return map.get(current_pos_key);
             }
             int previous_pos = x.get(x.size() - 2);
             
-            // En caso de no cumplir con la distancia mínima decidimos si reemplazar billboards
+            //En caso de no cumplir con la distancia mínima decidimos si reemplazar billboards
             if(current_pos - previous_pos <= min_distance){
-                /* Será 0 por defecto, en caso de no tener otro punto previo al
-                billboard anterior, el previous_max_rev permanecerá en 0 */
+                /*Será 0 por defecto, en caso de no tener otro punto previo al
+                billboard anterior, el previous_max_rev permanecerá en 0*/
                 int previous_max_rev = 0;
-                /* Max_rev previo a la colocación del billboard anterior, si no
-                hay no entra en el bucle quedando el previous_max_rev en 0 */
+                /*Max_rev previo a la colocación del billboard anterior, si no
+                hay no entra en el bucle quedando el previous_max_rev en 0*/
                 for (int i = 2; i < x.size(); i++) {
                     previous_pos = x.get(x.size() -i -1);
-                    /* El punto de comparación previo al billboard anterior debe
-                    ser un punto más allá de la distancia mínima */
+                    /*El punto de comparación previo al billboard anterior debe
+                    ser un punto más allá de la distancia mínima*/
                     if(current_pos - previous_pos > min_distance){
                         previous_max_rev = maxRevenue(cut(x,0,x.size()-i), cut(revenue,0,revenue.size()-i), min_distance);
                         break;
                     }
                 }
                 
-                // Reemplazamos billboard si el nuevo da mejor revenue
-                // Si no, no colocamos el billboard actual y mantenemos el antiguo
+                //Reemplazamos billboard si el nuevo da mejor revenue
+                //Si no, no colocamos el billboard actual y mantenemos el antiguo
                 map.put(current_pos_key, Math.max(previous_max_rev + revenue.get(revenue.size()-1), current_max_rev));
                 return map.get(current_pos_key);
             }
-            // Si cumple con la distancia simplemente lo colocamos
+            //Si cumple con la distancia simplemente lo colocamos
             map.put(current_pos_key, current_max_rev + revenue.get(revenue.size() - 1));
             return map.get(current_pos_key);
         }
